638094492f6db3e40a85daea4a98b0d8e7717569
sondeokhyeon <sondeokhyeon@gmail.com>
2023-01-06 14:25:08 +0900

    LSP saga short cut add

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -2,9 +2,7 @@ local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
 
 saga.init_lsp_saga {
-    server_filetype_map = {
-        typescript = 'typescript'
-    }
+    finder_request_timeout = 5000,
 }
 
 local opts = { noremap = true, silent = true }
@@ -13,3 +11,4 @@ vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
+vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
