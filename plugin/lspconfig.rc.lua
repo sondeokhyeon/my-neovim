7984d74ef8290084dec3e570183e1492c2d1ad9e
sondeokhyeon <sondeokhyeon@gmail.com>
2023-02-25 21:00:50 +0900

    Lua lsp config add, git ignore update

--- plugin/lspconfig.rc.lua
+++ plugin/lspconfig.rc.lua
@@ -88,23 +88,28 @@ nvim_lsp.sourcekit.setup {
     on_attach = on_attach,
 }
 
--- nvim_lsp.lua_language_server.setup {
---     on_attach = on_attach,
---     settings = {
---         Lua = {
---             diagnostics = {
---                 -- Get the language server to recognize the `vim` global
---                 globals = { 'vim' },
---             },
---
---             workspace = {
---                 -- Make the server aware of Neovim runtime files
---                 library = vim.api.nvim_get_runtime_file("", true),
---                 checkThirdParty = false
---             },
---         },
---     },
--- }
+nvim_lsp.lua_ls.setup {
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
 
 -- nvim_lsp.sumneko_lua.setup {
 --     on_attach = on_attach,
