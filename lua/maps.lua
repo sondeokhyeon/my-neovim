d98d489e9239b7423dfdd372a6bcf879dd6aa09e
sondeokhyeon <sondeokhyeon@gmail.com>
2023-04-04 22:38:10 +0900

    Add : auto number toggle setup

--- lua/maps.lua
+++ lua/maps.lua
@@ -32,3 +32,7 @@ keymap.set('n', '<C-w><left>', '<C-w><')
 keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
+
+-- number Toggle 
+keymap.set('n', '<Leader>[', ':set number norelativenumber')
+keymap.set('n', '<Leader>]', ':set number relativenumber')
