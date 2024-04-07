c62b2081c2acc83c59fca9e544c30398082082f9
sondeokhyeon <sondeokhyeon@gmail.com>
2024-04-07 15:53:49 +0900

    Modify : NeoTest adapter (jest, vitest, playwright, mocha)

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -5,6 +5,7 @@ return {
       "haydenmeade/neotest-jest",
       "marilari88/neotest-vitest",
       "thenbe/neotest-playwright",
+      "adrigzr/neotest-mocha",
     },
     keys = {
       {
@@ -30,22 +31,43 @@ return {
     opts = function(_, opts)
       table.insert(
         opts.adapters,
-        require("neotest-jest")(
-          {
-            jestCommand = "npm test --",
-            jestConfigFile = "custom.jest.config.ts",
-            env = { CI = true },
-            cwd = function()
-              return vim.fn.getcwd()
-            end,
+        require("neotest-jest")({
+          jestCommand = "npm test --",
+          jestConfigFile = "custom.jest.config.ts",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        })
+      )
+      table.insert(opts.adapters, require("neotest-vitest"))
+      table.insert(
+        opts.adapters,
+        require("neotest-playwright").adapter({
+          options = {
+            persist_project_selection = true,
+            enable_dynamic_test_discovery = true,
           },
-          require("neotest-playwright").adapter({
-            options = {
-              persist_project_selection = true,
-              enable_dynamic_test_discovery = true,
-            },
-          })
-        )
+        })
+      )
+      table.insert(
+        opts.adapters,
+        require("neotest-mocha")({
+          command = "npm test --",
+          command_args = function(context)
+            return {
+              -- "--full-trace",
+              -- "--reporter=json",
+              -- "--reporter-options=output=" .. context.results_path,
+              "--grep=" .. context.test_name_pattern,
+              context.path,
+            }
+          end,
+          env = { CI = true },
+          cwd = function(path)
+            return vim.fn.getcwd()
+          end,
+        })
       )
     end,
   },
