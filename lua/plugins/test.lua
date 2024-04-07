3106608f4d371e904924db1772509adae3dd82f8
sondeokhyeon <sondeokhyeon@gmail.com>
2024-04-07 11:16:39 +0900

    Add : NeoTest adapter (jet, vitest, playwright)

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,7 +1,52 @@
 return {
-  { "nvim-neotest/neotest-plenary" },
   {
     "nvim-neotest/neotest",
-    opts = { adapters = { "neotest-plenary" } },
+    dependencies = {
+      "haydenmeade/neotest-jest",
+      "marilari88/neotest-vitest",
+      "thenbe/neotest-playwright",
+    },
+    keys = {
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "Run Last Test",
+      },
+      {
+        "<leader>tL",
+        function()
+          require("neotest").run.run_last({ strategy = "dap" })
+        end,
+        desc = "Debug Last Test",
+      },
+      {
+        "<leader>tw",
+        "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
+        desc = "Run Watch",
+      },
+    },
+    opts = function(_, opts)
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")(
+          {
+            jestCommand = "npm test --",
+            jestConfigFile = "custom.jest.config.ts",
+            env = { CI = true },
+            cwd = function()
+              return vim.fn.getcwd()
+            end,
+          },
+          require("neotest-playwright").adapter({
+            options = {
+              persist_project_selection = true,
+              enable_dynamic_test_discovery = true,
+            },
+          })
+        )
+      )
+    end,
   },
 }
