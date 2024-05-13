67b56d08a3bd52614420fccf1c0e9f087b303c66
sondeokhyeon <sondeokhyeon@gmail.com>
2024-05-13 13:11:22 +0900

    Add : plugin - tmux navigator

---
+++ lua/plugins/tmux.lua
@@ -0,0 +1,18 @@
+return {
+  "alexghergh/nvim-tmux-navigation",
+  event = "VeryLazy",
+  config = function()
+    local nvim_tmux_nav = require("nvim-tmux-navigation")
+    nvim_tmux_nav.setup({
+      disable_when_zoomed = true,
+      -- defaults to false
+      keybindings = {
+        left = "<C-H>",
+        down = "<C-J>",
+        up = "<C-K>",
+        right = "<C-L>",
+        next = "<C-Space>",
+      },
+    })
+  end,
+}
