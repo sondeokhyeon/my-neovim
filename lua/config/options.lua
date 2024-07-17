cc681198b784e19852822907d3f45ba1ab9f4b51
sondeokhyeon <sondeokhyeon@gmail.com>
2024-07-17 23:41:49 +0900

    Modify : nvim Mouse wheel option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -37,8 +37,8 @@ vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
 vim.opt.wildignore:append({ "*/node_modules/*" })
 vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
-vim.opt.splitkeep = "cursor"
-vim.opt.mouse = ""
+-- vim.opt.splitkeep = "cursor"
+-- vim.opt.mouse = ""
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
