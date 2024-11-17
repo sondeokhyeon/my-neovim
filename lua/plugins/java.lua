6001a8c8fcace124e97ca1d37e10032065de1d00
sondeokhyeon <sondeokhyeon@gmail.com>
2024-11-17 22:26:27 +0900

    Modify : Java LSP seperate

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,3 +1,9 @@
 return {
   "nvim-java/nvim-java",
+  config = function()
+    local lspConfig = require("lspconfig")
+    local java = require("java")
+    java.setup()
+    lspConfig.jdtls.setup({})
+  end,
 }
