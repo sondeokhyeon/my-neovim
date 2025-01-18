2dbc35a6d5652b527d26a50e7576c8d7fb4ad375
sondeokhyeon <sondeokhyeon@gmail.com>
2025-01-18 23:17:40 +0900

    [nvim][plugin] : nvimCmp plugin add

---
+++ lua/plugins/nvimCmp.lua
@@ -0,0 +1,14 @@
+return {
+  -- Autocompletion
+  -- "hrsh7th/nvim-cmp",
+  -- config = function()
+  --   require("cmp").setup({
+  --     sources = {
+  --       { name = "nvim_lsp" },
+  --       { name = "snippets", keyword_length = 3 },
+  --       { name = "path" },
+  --       { name = "buffer" },
+  --     },
+  --   })
+  -- end,
+}
