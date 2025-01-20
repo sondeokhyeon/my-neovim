ea87acc9f2796edebd80947a4eb08c9ff74f20da
sondeokhyeon <sondeokhyeon@gmail.com>
2025-01-20 23:58:21 +0900

    [wezterm][dot] : dot file update
    1. grouping apply
    2. comment add
    3. windows wsl option add

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,24 +2,30 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+-- ** only Windows OS Options **
+-- current Domain
+-- config.default_domain = "WSL:Ubuntu"
+
+-- tab
 config.enable_tab_bar = true
 
+-- window deco
 config.window_decorations = "RESIZE"
+config.enable_tab_bar = true
 
+-- font size
 config.font_size = 14
-
--- config.font = wezterm.font("D2Coding Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
-
 config.font = wezterm.font_with_fallback({
-  {
-    family = "JetBrains Mono",
-    weight = "Regular",
-    harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-  },
+	{
+		family = "JetBrains Mono",
+		weight = "Regular",
+		harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+	},
 })
-
+-- config.font = wezterm.font("D2Coding Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
 -- config.font = wezterm.font({ weight = "Medium", stretch = "Normal", style = "Normal" })
 
+-- colorscheme
 config.color_scheme = "Tokyo Night Day"
 
 -- config.unix_domains = { {
@@ -27,6 +33,8 @@ config.color_scheme = "Tokyo Night Day"
 -- } }
 
 -- config.default_gui_startup_args = { "connect", "unix" }
+
+-- opacity
 -- config.window_background_opacity = 0.5
 -- config.macos_window_background_blur = 0.5
 
