2aaff13d2bcf4f2791507208660831acd5e074f3
sondeokhyeon <sondeokhyeon@gmail.com>
2025-04-18 10:33:35 +0900

    [nvim][plugin] : lazyvim extra plugin add

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -8,154 +8,157 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-  spec = {
-    -- add LazyVim and import its plugins
-    {
-      "LazyVim/LazyVim",
-      import = "lazyvim.plugins",
-      opts = {
-        colorscheme = "tokyonight-day",
-        -- news = {
-        --   lazyvim = true,
-        --   neovim = true,
-        -- },
-      },
-    },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "plugins" },
-  },
-  defaults = {
-    autocmds = true, -- lazyvim.config.autocmds
-    keymaps = true, -- lazyvim.config.keymaps
-    lazy = false,
-    version = false, -- always use the latest git commit
-  },
-  news = {
-    lazyvim = true,
-    neovim = false,
-  },
-  checker = { enabled = true }, -- automatically check for plugin updates
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
-  icons = {
-    misc = {
-      dots = "󰇘",
-    },
-    ft = {
-      octo = "",
-    },
-    dap = {
-      Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
-      Breakpoint = " ",
-      BreakpointCondition = " ",
-      BreakpointRejected = { " ", "DiagnosticError" },
-      LogPoint = ".>",
-    },
-    diagnostics = {
-      Error = " ",
-      Warn = " ",
-      Hint = " ",
-      Info = " ",
-    },
-    git = {
-      added = " ",
-      modified = " ",
-      removed = " ",
-    },
-    kinds = {
-      Array = " ",
-      Boolean = "󰨙 ",
-      Class = " ",
-      Codeium = "󰘦 ",
-      Color = " ",
-      Control = " ",
-      Collapsed = " ",
-      Constant = "󰏿 ",
-      Constructor = " ",
-      Copilot = " ",
-      Enum = " ",
-      EnumMember = " ",
-      Event = " ",
-      Field = " ",
-      File = " ",
-      Folder = " ",
-      Function = "󰊕 ",
-      Interface = " ",
-      Key = " ",
-      Keyword = " ",
-      Method = "󰊕 ",
-      Module = " ",
-      Namespace = "󰦮 ",
-      Null = " ",
-      Number = "󰎠 ",
-      Object = " ",
-      Operator = " ",
-      Package = " ",
-      Property = " ",
-      Reference = " ",
-      Snippet = "󱄽 ",
-      String = " ",
-      Struct = "󰆼 ",
-      Supermaven = " ",
-      TabNine = "󰏚 ",
-      Text = " ",
-      TypeParameter = " ",
-      Unit = " ",
-      Value = " ",
-      Variable = "󰀫 ",
-    },
-  },
-  ---@type table<string, string[]|boolean>?
-  kind_filter = {
-    default = {
-      "Class",
-      "Constructor",
-      "Enum",
-      "Field",
-      "Function",
-      "Interface",
-      "Method",
-      "Module",
-      "Namespace",
-      "Package",
-      "Property",
-      "Struct",
-      "Trait",
-    },
-    markdown = false,
-    help = false,
-    -- you can specify a different filter for each filetype
-    lua = {
-      "Class",
-      "Constructor",
-      "Enum",
-      "Field",
-      "Function",
-      "Interface",
-      "Method",
-      "Module",
-      "Namespace",
-      -- "Package", -- remove package since luals uses it for control flow structures
-      "Property",
-      "Struct",
-      "Trait",
-    },
-  },
+	spec = {
+		-- add LazyVim and import its plugins
+		{
+			"LazyVim/LazyVim",
+			import = "lazyvim.plugins",
+			opts = {
+				colorscheme = "tokyonight-day",
+				-- news = {
+				--   lazyvim = true,
+				--   neovim = true,
+				-- },
+			},
+		},
+		{ import = "lazyvim.plugins.extras.linting.eslint" },
+		{ import = "lazyvim.plugins.extras.formatting.prettier" },
+		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.lang.json" },
+		-- { import = "lazyvim.plugins.extras.coding.copilot" },
+		{
+			import = "lazyvim.plugins.extras.lsp.none-ls",
+		},
+		{ import = "plugins" },
+	},
+	defaults = {
+		autocmds = true, -- lazyvim.config.autocmds
+		keymaps = true, -- lazyvim.config.keymaps
+		lazy = false,
+		version = false, -- always use the latest git commit
+	},
+	news = {
+		lazyvim = true,
+		neovim = false,
+	},
+	checker = { enabled = true }, -- automatically check for plugin updates
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+	icons = {
+		misc = {
+			dots = "󰇘",
+		},
+		ft = {
+			octo = "",
+		},
+		dap = {
+			Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+			Breakpoint = " ",
+			BreakpointCondition = " ",
+			BreakpointRejected = { " ", "DiagnosticError" },
+			LogPoint = ".>",
+		},
+		diagnostics = {
+			Error = " ",
+			Warn = " ",
+			Hint = " ",
+			Info = " ",
+		},
+		git = {
+			added = " ",
+			modified = " ",
+			removed = " ",
+		},
+		kinds = {
+			Array = " ",
+			Boolean = "󰨙 ",
+			Class = " ",
+			Codeium = "󰘦 ",
+			Color = " ",
+			Control = " ",
+			Collapsed = " ",
+			Constant = "󰏿 ",
+			Constructor = " ",
+			Copilot = " ",
+			Enum = " ",
+			EnumMember = " ",
+			Event = " ",
+			Field = " ",
+			File = " ",
+			Folder = " ",
+			Function = "󰊕 ",
+			Interface = " ",
+			Key = " ",
+			Keyword = " ",
+			Method = "󰊕 ",
+			Module = " ",
+			Namespace = "󰦮 ",
+			Null = " ",
+			Number = "󰎠 ",
+			Object = " ",
+			Operator = " ",
+			Package = " ",
+			Property = " ",
+			Reference = " ",
+			Snippet = "󱄽 ",
+			String = " ",
+			Struct = "󰆼 ",
+			Supermaven = " ",
+			TabNine = "󰏚 ",
+			Text = " ",
+			TypeParameter = " ",
+			Unit = " ",
+			Value = " ",
+			Variable = "󰀫 ",
+		},
+	},
+	---@type table<string, string[]|boolean>?
+	kind_filter = {
+		default = {
+			"Class",
+			"Constructor",
+			"Enum",
+			"Field",
+			"Function",
+			"Interface",
+			"Method",
+			"Module",
+			"Namespace",
+			"Package",
+			"Property",
+			"Struct",
+			"Trait",
+		},
+		markdown = false,
+		help = false,
+		-- you can specify a different filter for each filetype
+		lua = {
+			"Class",
+			"Constructor",
+			"Enum",
+			"Field",
+			"Function",
+			"Interface",
+			"Method",
+			"Module",
+			"Namespace",
+			-- "Package", -- remove package since luals uses it for control flow structures
+			"Property",
+			"Struct",
+			"Trait",
+		},
+	},
 })
