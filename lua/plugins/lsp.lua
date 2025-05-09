5de0d54dc7384c804b8009d4c7836084c0464146
sondeokhyeon <sondeokhyeon@gmail.com>
2025-05-09 18:06:33 +0900

    [nvim][plugin] : copilot, mason, lualine update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,146 +1,132 @@
 return {
 	-- tools
+	-- "mason-org/mason.nvim",
+	-- "mason-org/mason-lspconfig.nvim",
+	-- { "mason-org/mason.nvim", version = "0.11.0" },
+	-- { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+	{ "mason-org/mason.nvim", version = "^1.0.0" },
+	{ "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+	-- lsp servers
 	{
-		"williamboman/mason.nvim",
+		"neovim/nvim-lspconfig",
+		---@class PluginLspOpts
+		-- dependencies = {
+		--   "mfussenegger/nvim-jdtls", -- or nvim-java, nvim-lspconfig
+		-- },
+		-- config = function()
+		-- local lspConfig = require("lspconfig")
+		-- local java = require("java")
+		-- java.setup()
+		-- lspConfig.jdtls.setup({})
+		-- end,
 		opts = {
-			ensure_installed = {
-				"stylua",
-				"selene",
-				"luacheck",
-				"shellcheck",
-				"shfmt",
-				"tailwindcss-language-server",
-				"typescript",
-				"css-lsp",
-				"cssmodules-language-server",
-				"tsx",
-				"vim",
-				"prettier",
-				"vtsls",
+			diagnostics = {
+				underline = true,
+				update_in_insert = false,
+				virtual_text = {
+					spacing = 4,
+					source = "if_many",
+					prefix = "●",
+				},
+				severity_sort = true,
 			},
-		},
-		-- lsp servers
-		{
-			"neovim/nvim-lspconfig",
-			---@class PluginLspOpts
-			-- dependencies = {
-			--   "mfussenegger/nvim-jdtls", -- or nvim-java, nvim-lspconfig
-			-- },
-			-- config = function()
-			-- local lspConfig = require("lspconfig")
-			-- local java = require("java")
-			-- java.setup()
-			-- lspConfig.jdtls.setup({})
-			-- end,
-			opts = {
-				diagnostics = {
-					underline = true,
-					update_in_insert = false,
-					virtual_text = {
-						spacing = 4,
-						source = "if_many",
-						prefix = "●",
+			inlay_hints = {
+				enabled = true,
+			},
+			-- add any global capabilities here
+			capabilities = vim.lsp.protocol.make_client_capabilities(),
+			-- LSP Server Settings
+			servers = {
+				lua_ls = {
+					-- keys = {},
+					settings = {
+						Lua = {
+							workspace = {
+								checkThirdParty = false,
+							},
+							completion = {
+								callSnippet = "Replace",
+							},
+						},
 					},
-					severity_sort = true,
 				},
-				inlay_hints = {
-					enabled = true,
+				-- lua_ls
+				cssls = {},
+				-- cssls
+				tailwindcss = {
+					root_dir = function(...)
+						return require("lspconfig.util").root_pattern(".git")(...)
+					end,
 				},
-				-- add any global capabilities here
-				capabilities = vim.lsp.protocol.make_client_capabilities(),
-				-- LSP Server Settings
-				servers = {
-					lua_ls = {
-						-- keys = {},
-						settings = {
-							Lua = {
-								workspace = {
-									checkThirdParty = false,
-								},
-								completion = {
-									callSnippet = "Replace",
-								},
+				--tailwindcss
+				tsserver = {
+					root_dir = function(...)
+						return require("lspconfig.util").root_pattern(".git")(...)
+					end,
+					filetypes = {
+						"javascript",
+						"javascriptreact",
+						"javascript.jsx",
+						"typescript",
+						"typescriptreact",
+						"typescript.tsx",
+					},
+					-- cmd = { "typescript-language-server", "--stdio" },
+					init_options = {
+						preferences = {
+							includeInlayParameterNameHints = "all",
+							includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+							includeInlayFunctionParameterTypeHints = true,
+							includeInlayVariableTypeHints = true,
+							includeInlayPropertyDeclarationTypeHints = true,
+							includeInlayFunctionLikeReturnTypeHints = true,
+							includeInlayEnumMemberValueHints = true,
+							importModuleSpecifierPreference = "non-relative",
+						},
+					},
+					single_file_support = true,
+				},
+				-- tsserver
+				html = {},
+				-- html
+				emmet_language_server = {},
+				-- emmet ls
+				yamlls = {
+					settings = {
+						yaml = {
+							schemas = {
+								["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
+								["https://json.schemastore.org/ansible-stable-2.9.json"] = "/*.ansible.yaml",
+								["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+								-- schemas = {
+								--   kubernetes = "/*.k8s.yaml",
+								--   ansible = "/*.ansible.yaml",
+								--   github = "/.github/workflows/*",
+								-- },
 							},
 						},
 					},
-					-- lua_ls
-					cssls = {},
-					-- cssls
-					tailwindcss = {
-						root_dir = function(...)
-							return require("lspconfig.util").root_pattern(".git")(...)
-						end,
-					},
-					--tailwindcss
-					tsserver = {
-						root_dir = function(...)
-							return require("lspconfig.util").root_pattern(".git")(...)
-						end,
-						filetypes = {
-							"javascript",
-							"javascriptreact",
-							"javascript.jsx",
-							"typescript",
-							"typescriptreact",
-							"typescript.tsx",
-						},
-						-- cmd = { "typescript-language-server", "--stdio" },
-						init_options = {
-							preferences = {
-								includeInlayParameterNameHints = "all",
-								includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-								includeInlayFunctionParameterTypeHints = true,
-								includeInlayVariableTypeHints = true,
-								includeInlayPropertyDeclarationTypeHints = true,
-								includeInlayFunctionLikeReturnTypeHints = true,
-								includeInlayEnumMemberValueHints = true,
-								importModuleSpecifierPreference = "non-relative",
-							},
-						},
-						single_file_support = true,
-					},
-					-- tsserver
-					html = {},
-					-- html
-					emmet_language_server = {},
-					-- emmet ls
-					yamlls = {
-						settings = {
-							yaml = {
-								schemas = {
-									["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
-									["https://json.schemastore.org/ansible-stable-2.9.json"] = "/*.ansible.yaml",
-									["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-									-- schemas = {
-									--   kubernetes = "/*.k8s.yaml",
-									--   ansible = "/*.ansible.yaml",
-									--   github = "/.github/workflows/*",
-									-- },
-								},
-							},
-						},
-					},
-					-- yamlls
-					docker_compose_language_service = {
-						cmd = { "docker-compose-language-server", "--stdio" },
-						filetypes = { "yaml.docker-compose" },
-						root_pattern = { "docker-compose.yaml", "docker-compose.yml", "compose.yaml", "compose.yml" },
-						single_file_support = true,
-					},
-					-- docker_compose_language_service
-					dockerls = {
-						cmd = { "docker-langserver", "--stdio" },
-						filetypes = { "dockerfile" },
-						root_pattern = { "Dockerfile" },
-						single_file_support = true,
-					},
-					-- dockerls end
-					-- kotlin_language_server = {
-					--   cmd = { "kotlin-language-server", "--stdio" },
-					--   filetypes = { "kotlin" },
-					-- },
-				}, -- servers
-			}, -- opts
-		},
+				},
+				-- yamlls
+				docker_compose_language_service = {
+					cmd = { "docker-compose-language-server", "--stdio" },
+					filetypes = { "yaml.docker-compose" },
+					root_pattern = { "docker-compose.yaml", "docker-compose.yml", "compose.yaml", "compose.yml" },
+					single_file_support = true,
+				},
+				-- docker_compose_language_service
+				dockerls = {
+					cmd = { "docker-langserver", "--stdio" },
+					filetypes = { "dockerfile" },
+					root_pattern = { "Dockerfile" },
+					single_file_support = true,
+				},
+				-- dockerls end
+				-- kotlin_language_server = {
+				--   cmd = { "kotlin-language-server", "--stdio" },
+				--   filetypes = { "kotlin" },
+				-- },
+			}, -- servers
+		}, -- opts
 	},
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,119 +1,136 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "b0o/incline.nvim",
-    dependencies = { "craftzdog/solarized-osaka.nvim" },
-    event = "BufReadPre",
-    priority = 1200,
-    config = function()
-      -- local colors = require("solarized-osaka.colors").setup()
-      require("incline").setup({
-        -- highlight = {
-        --   groups = {
-        --     InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
-        --     InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
-        --   },
-        -- },
-        window = { margin = { vertical = 0, horizontal = 1 } },
-        hide = {
-          cursorline = true,
-        },
-        render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if vim.bo[props.buf].modified then
-            filename = "[+] " .. filename
-          end
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("lualine").setup({
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+					lualine_c = {
+						{
+							"filename",
+							path = 1, -- 여기에 path 옵션을 줌
+						},
+					},
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+			})
+		end,
+	},
+	{
+		"b0o/incline.nvim",
+		dependencies = { "craftzdog/solarized-osaka.nvim" },
+		event = "BufReadPre",
+		priority = 1200,
+		config = function()
+			-- local colors = require("solarized-osaka.colors").setup()
+			require("incline").setup({
+				-- highlight = {
+				--   groups = {
+				--     InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+				--     InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+				--   },
+				-- },
+				window = { margin = { vertical = 0, horizontal = 1 } },
+				hide = {
+					cursorline = true,
+				},
+				render = function(props)
+					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+					if vim.bo[props.buf].modified then
+						filename = "[+] " .. filename
+					end
 
-          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-          return { { icon, guifg = color }, { " " }, { filename } }
-        end,
-      })
-    end,
-  },
-  {
-    "akinsho/bufferline.nvim",
-    version = "*",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    event = "VeryLazy",
-    keys = {
-      -- { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-      -- { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-    },
-    opts = {
-      options = {
-        mode = "tabs",
-        separator_style = "thin",
-        show_buffer_close_icons = true,
-        show_close_icon = false,
-        show_buffer_icons = true,
-        show_tab_indicators = true,
-        enforce_regular_tabs = true,
-        always_show_bufferline = true,
-        auto_toggle_bufferline = true,
-        themable = true,
-        toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
-      },
-    },
-  },
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 10000,
-    },
-  },
-  {
-    "folke/noice.nvim",
-    opts = function(_, opts)
-      table.insert(opts.routes, {
-        filter = {
-          event = "notify",
-          find = "No information available",
-        },
-        opts = { skip = true },
-      })
-      local focused = true
-      vim.api.nvim_create_autocmd("FocusGained", {
-        callback = function()
-          focused = true
-        end,
-      })
-      vim.api.nvim_create_autocmd("FocusLost", {
-        callback = function()
-          focused = false
-        end,
-      })
-      table.insert(opts.routes, 1, {
-        filter = {
-          cond = function()
-            return not focused
-          end,
-        },
-        view = "notify_send",
-        opts = { stop = false },
-      })
+					local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+					return { { icon, guifg = color }, { " " }, { filename } }
+				end,
+			})
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		event = "VeryLazy",
+		keys = {
+			-- { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+			-- { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+		},
+		opts = {
+			options = {
+				mode = "tabs",
+				separator_style = "thin",
+				show_buffer_close_icons = true,
+				show_close_icon = false,
+				show_buffer_icons = true,
+				show_tab_indicators = true,
+				enforce_regular_tabs = true,
+				always_show_bufferline = true,
+				auto_toggle_bufferline = true,
+				themable = true,
+				toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+			},
+		},
+	},
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 10000,
+		},
+	},
+	{
+		"folke/noice.nvim",
+		opts = function(_, opts)
+			table.insert(opts.routes, {
+				filter = {
+					event = "notify",
+					find = "No information available",
+				},
+				opts = { skip = true },
+			})
+			local focused = true
+			vim.api.nvim_create_autocmd("FocusGained", {
+				callback = function()
+					focused = true
+				end,
+			})
+			vim.api.nvim_create_autocmd("FocusLost", {
+				callback = function()
+					focused = false
+				end,
+			})
+			table.insert(opts.routes, 1, {
+				filter = {
+					cond = function()
+						return not focused
+					end,
+				},
+				view = "notify_send",
+				opts = { stop = false },
+			})
 
-      opts.commands = {
-        all = {
-          -- options for the message history that you get with `:Noice`
-          view = "split",
-          opts = { enter = true, format = "details" },
-          filter = {},
-        },
-      }
+			opts.commands = {
+				all = {
+					-- options for the message history that you get with `:Noice`
+					view = "split",
+					opts = { enter = true, format = "details" },
+					filter = {},
+				},
+			}
 
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "markdown",
-        callback = function(event)
-          vim.schedule(function()
-            require("noice.text.markdown").keys(event.buf)
-          end)
-        end,
-      })
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "markdown",
+				callback = function(event)
+					vim.schedule(function()
+						require("noice.text.markdown").keys(event.buf)
+					end)
+				end,
+			})
 
-      opts.presets.lsp_doc_border = true
-    end,
-  },
+			opts.presets.lsp_doc_border = true
+		end,
+	},
 }
